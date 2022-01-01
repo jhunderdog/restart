521c8561d17f74a198f16134168f34ea12cbf37a
jhunderdog <33382water@gmail.com>
2022-01-01 15:10:39 +0900

    first commit

---
+++ Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by jhunderdog on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Students/Restart/Restart/Screens/HomeView.swift
@@ -0,0 +1,61 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by jhunderdog on 2021/12/30.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    var body: some View {
+        VStack(spacing: 20) {
+            //MARK - HEADER
+            Spacer()
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+            //MARK - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            //MARK - FOOTER
+            Spacer()
+            
+            
+            
+            
+            
+            Button(action:{
+                isOnboardingViewActive = true
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        }
+    }
+}
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
